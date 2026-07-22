14b2bec3231d803b0c52be4d6ba08a1fdf870424
Ibrahim <ibrahimsiddique339@gmail.com>
2026-07-23 02:20:42 +0530

    increment operators

---
+++ increment_operators.java
@@ -0,0 +1,18 @@
+public class increment_operators {
+    public static void main(String[] args) {
+        // Increment and Decrement Operators
+        // if only incrementing by one
+        
+        
+        int x = 1;
+        // x = x = 1;
+        
+        x++;
+        x++;
+        x++;
+        x++;
+
+        System.out.println(x);
+
+    }
+}
