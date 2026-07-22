62e19824effebdca105da7c0a10b04db424e389f
Ibrahim <ibrahimsiddique339@gmail.com>
2026-07-23 02:19:21 +0530

    arithmetic operators

--- arithmetic_operator.java
+++ arithmetic_operator.java
@@ -1,5 +1,19 @@
 public class arithmetic_operator {
     public static void main(String[] args) {
         
+        // Arithmetic Operators
+
+        int x = 10;
+        int y = 3;
+        double z;
+
+        // z = x + y;
+        // z = x - y;
+        // z = x * y;
+        // z = x / y;
+        
+        z = x % y;
+
+        System.out.println(z);
     }
 }
