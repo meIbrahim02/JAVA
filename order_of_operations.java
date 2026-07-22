a82550a1d20eb37c4f130e0e6cd31b7930ce8426
Ibrahim <ibrahimsiddique339@gmail.com>
2026-07-23 02:22:24 +0530

    order of operations

---
+++ order_of_operations.java
@@ -0,0 +1,9 @@
+public class order_of_operations {
+    public static void main(String[] args) {
+        // order of operations [P-E-M-D-A-S]
+
+        double result = 3 + 4 * (7 - 5) / 2.0;
+        System.out.println(result);
+    }
+}
+ 
