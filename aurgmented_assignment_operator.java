f39b8f35a3743defbcdedf50b9583c8d2f9dfbf3
Ibrahim <ibrahimsiddique339@gmail.com>
2026-07-23 02:20:05 +0530

    augmented assignment operators

---
+++ aurgmented_assignment_operator.java
@@ -0,0 +1,18 @@
+public class aurgmented_assignment_operator {
+    public static void main(String[] args) {
+       // Augmented Assignment Operators
+       
+       int x = 10;
+       int y = 3;
+
+       // x = x + y; or x += y
+       // x = x - y; or x -= y
+       // x = x * y; or x *= y
+       // x = x / y; or x /= y
+       // x = x % y; or x %= y
+        
+       x += y;
+
+       System.out.println(x);
+    }
+}
