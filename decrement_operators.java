0478f46b918605a4bfb6455343262a6344d38510
Ibrahim <ibrahimsiddique339@gmail.com>
2026-07-23 02:21:53 +0530

    decrement operators

---
+++ decrement_operators.java
@@ -0,0 +1,16 @@
+public class decrement_operators {
+    public static void main(String[] args) {
+        // Decrement Operators
+        // to subtract one 
+
+        int x = 1;
+        // x = x - 1;
+
+        x--;
+        x--;
+        x--;
+        
+
+        System.out.println(x);
+    }
+}
