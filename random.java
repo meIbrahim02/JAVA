4b2eb077e76f27912150e8b0617bc2a22dc8f77a
Ibrahim Siddique <ibrahimsiddique0202@gmail.com>
2026-07-25 19:59:41 +0530

    ramdom complete

--- random.java
+++ random.java
@@ -1,5 +1,35 @@
+import java.util.Random;
 public class random {
     public static void main(String[] args) {
-        
+        Random random = new Random();
+
+        // for inteagers
+        int number1;
+        int number2;
+        int number3;
+
+        number1 = random.nextInt(1, 101);
+        number2 = random.nextInt(1, 101);
+        number3 = random.nextInt(1, 101);
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+
+        // double 
+        double number4;
+        number4 = random.nextDouble(1, 60);
+        System.out.println(number4);
+
+        boolean isHeads;
+        isHeads = random.nextBoolean();
+        System.out.println(isHeads);
+        if(isHeads){
+            System.out.println("HEADS");
+        }
+        else{
+            System.out.println("TAILS");
+        }
+
     }
 }
