ecc415163038efceaf7520bae73799a5f3673398
Ibrahim Siddique <ibrahimsiddique0202@gmail.com>
2026-07-25 20:15:24 +0530

    maths in java

---
+++ math.java
@@ -0,0 +1,20 @@
+public class math {
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        System.out.println(Math.E);
+        
+        double result;
+        result = Math.pow(2, 4);
+        result = Math.abs(-5); // only +
+        result = Math.sqrt(9);
+        result = Math.round(3.14);
+        result = Math.ceil(3.14);
+        result = Math.floor(9.78);
+        result = Math.max(10, 20);
+        result = Math.min(10, 20);
+        
+        System.out.println(result);
+
+
+    }
+}
