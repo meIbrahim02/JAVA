26d7e6b365c3c2f3bd140c79ea9cd07f0c946be1
Ibrahim Siddique <ibrahimsiddique0202@gmail.com>
2026-08-03 14:56:29 +0530

    call by value

---
+++ 04_function and methods/call_by_value.java
@@ -0,0 +1,20 @@
+public class call_by_value {
+
+    public static void swap(int a, int b){
+         // swap
+        int temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+    public static void main(String[] args) {
+        // swap - value exchange
+        int a = 5;
+        int b = 10;
+        swap(a, b);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+    }
+}
