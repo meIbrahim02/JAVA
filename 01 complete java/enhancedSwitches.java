9c22a33a14b591018d72ce4bb60d0cc4c712e0e0
Ibrahim Siddique <ibrahimsiddique0202@gmail.com>
2026-08-20 02:22:41 +0530

    enhanced switches

--- 01 complete java/enhancedSwitches.java
+++ 01 complete java/enhancedSwitches.java
@@ -1,5 +1,21 @@
+import java.util.*;
+
 public class enhancedSwitches {
     public static void main(String[] args) {
-        
+        // enhanced switches = a replacement to many if else statements
+        // introduced in java 14
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the day of the week");
+        String day = sc.nextLine();
+
+        switch(day){
+            case "monday", "tuesday", "wednesday", "thrusday" -> System.out.println("it is a weekday😒");
+            case "saturday", "sunday" -> System.out.println("it is weekend");
+            default -> System.out.println(day + " is not a day");
+        }
+
+        sc.close();
     }
 }
