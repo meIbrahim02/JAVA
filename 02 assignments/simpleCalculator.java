86d30a81c89e80fdd4675b719706f348790bbb12
Ibrahim Siddique <ibrahimsiddique0202@gmail.com>
2026-08-21 01:05:46 +0530

    simple calculator

---
+++ 02 assignments/simpleCalculator.java
@@ -0,0 +1,44 @@
+import java.util.*;
+public class simpleCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double num1;
+        double num2;
+        char operator;
+        double result = 0;
+        boolean validOperation = true;
+
+
+        System.out.print("Enter the first number: ");
+        num1 = sc.nextDouble();
+        System.out.print("Enter an operator (+,-,*,/,^): ");
+        operator = sc.next().charAt(0);
+        System.out.print("Enter the second number: ");
+        num2 = sc.nextDouble();
+
+        switch(operator){
+            case '+' -> result = num1 + num2;
+            case '-' -> result = num1 - num2;
+            case '*' -> result = num1 * num2;
+            case '/' -> {
+                if(num2 == 0){
+                    System.out.println("cannot divide by 0");
+                    validOperation = false;
+                }else{
+                    result = num1 / num2;
+                }
+            }
+            case '^' -> result = Math.pow(num1, num2);
+            default -> {
+                System.out.println("Invalid Operation");
+                validOperation = false;
+            }
+        }
+
+        if(validOperation){
+            System.out.println(result);
+        }
+
+        sc.close();
+    }
+}
