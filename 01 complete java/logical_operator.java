0ad2f89a8bda3bcdb0404aee1472fe4fac2198db
Ibrahim Siddique <ibrahimsiddique0202@gmail.com>
2026-08-21 01:20:42 +0530

    logical_operators

---
+++ 01 complete java/logical_operator.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class logical_operator {
+    public static void main(String[] args) {
+        // && = AND
+        // || = OR
+        // !  = NOT
+
+        double temp = -10 ;
+        boolean isSunny = true;
+
+        if(temp <= 30 && temp >= 0 && isSunny){
+            System.out.println("the weather is good");
+            System.out.println("the weather is Sunny");
+        }
+        else if(temp <= 30 && temp >= 0 && !isSunny){
+            System.out.println("the weather is good but NOT sunny");
+        }
+        else if(temp > 30 || temp < 0){
+            System.out.println("the temperature is bad");
+        }
+    }
+}
