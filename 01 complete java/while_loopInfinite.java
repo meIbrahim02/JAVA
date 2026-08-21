b8cb67bb4523dfa459c95afce855406df5eb0c2b
Ibrahim Siddique <ibrahimsiddique0202@gmail.com>
2026-08-22 01:54:44 +0530

    infinite loop

---
+++ 01 complete java/while_loopInfinite.java
@@ -0,0 +1,8 @@
+public class while_loopInfinite {
+    public static void main(String[] args) {
+        // never run this
+        while(1 == 1){
+            System.out.print("help me ");
+        }
+    }
+}
