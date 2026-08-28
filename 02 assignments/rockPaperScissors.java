c84f4c1ada593a014bbf61bbd4c320c0d8eeb5b3
Ibrahim Siddique <ibrahimsiddique0202@gmail.com>
2026-08-29 01:15:20 +0530

    complete rock paper scissors game

--- 02 assignments/rockPaperScissors.java
+++ 02 assignments/rockPaperScissors.java
@@ -12,22 +12,40 @@ public class rockPaperScissors {
         String playAgain = "yes";
 
         // get a choice from the user
-        System.out.print("enter your move (rock, paper, scissors) : ");
+        do{
+            System.out.print("enter your move (rock, paper, scissors) : ");
         playerChoice = sc.nextLine().toLowerCase();
 
         if(!playerChoice.equals("rock") && 
                 !playerChoice.equals("paper") && 
                     !playerChoice.equals("scissors")){
             System.out.println("Invalid ");
+            continue;
         }
 
         // get random choice for the computer
         computerChoice = userChoice[random.nextInt(3)];
         System.out.println("computer choice: " + computerChoice);
-        
+
         // check win conditons
+        if(playerChoice.equals(computerChoice)){
+            System.out.println("its a tie!");
+        }else if((playerChoice.equals("rock") && computerChoice.equals("scissors")) || 
+                    (playerChoice.equals("paper") && computerChoice.equals("rock") || 
+                        (playerChoice.equals("scissors") && computerChoice.equals("paper")))){
+            System.out.println("you win");    
+        }else{
+            System.out.println("you lose!");
+        }
         // ask to play again
+        System.out.println("play again (yes/no): ");
+        playAgain = sc.nextLine().toLowerCase();
+
+
+        } while(playAgain.equals("yes"));
         // goodbye message
-        sc.close();
+        System.out.println("thanks for playing");
+    sc.close();
+        
     }
 }
