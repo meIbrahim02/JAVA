e44c1a196f9a2f58627b7821c84c96615a5d4dac
Ibrahim Siddique <ibrahimsiddique0202@gmail.com>
2026-09-04 16:58:18 +0530

    Abstract

---
+++ 03 object oriented programming/10 Abstration/Shape.java
@@ -0,0 +1,8 @@
+public abstract class Shape {
+    
+    abstract double area(); // ABSTRACT
+
+    void display(){ //Concrete
+        System.out.println("This is a shape");
+    }
+}
---
+++ 03 object oriented programming/10 Abstration/main.java
@@ -0,0 +1,22 @@
+public class main{
+    public static void main(String[] args) {
+        // abstract = Used to define abstract classes and methods. 
+        //              Abstraction is the process of hiding implementation details
+        //              and showing only the essential features
+        //             Abstract classes CAN'T be instantiated directly
+        //              can contain 'abstract' methods (which must be implemented)
+        //              can contain 'concrete' methods (ehich are inherited)
+
+        Circle circle = new Circle(3);
+        Triangle triangle = new Triangle(4, 5);
+        Rectangle rectangle = new Rectangle(6, 7);
+
+        circle.display();
+        triangle.display();
+        rectangle.display();
+
+        System.out.println(circle.area());
+        System.out.println(triangle.area());
+        System.out.println(rectangle.area());
+    }
+}
